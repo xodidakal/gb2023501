b32bef5219c9c49cbbce76ad2fd7784c25a29317
admin <admin@DESKTOP-UU4I217>
2023-12-06 15:10:47 +0900

    용용

--- gb2023501/src/main/java/com/choongang/gb2023501/controller/YbController.java
+++ gb2023501/src/main/java/com/choongang/gb2023501/controller/YbController.java
@@ -1,11 +1,26 @@
 package com.choongang.gb2023501.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class YbController {
-
+	@RequestMapping(value = "/")
+	public String main() {
+		System.out.println("Main start...");
+		return "main";
+	}
+	@RequestMapping(value = "eduResourceForm")
+	public String eduResourceForm() {
+		System.out.println("ybController eduResourceForm start...");
+		return "yb/eduResourceForm";
+	}
+	@RequestMapping(value = "eduResourceList")
+	public String eduResourceList() {
+		System.out.println("ybController eduResourceList start...");
+		return "yb/eduResourceList";
+	}
 }
