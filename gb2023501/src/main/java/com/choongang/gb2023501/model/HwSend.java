e5a38e0381ae838ce81c7de9d09f210b9a4c8c2d
admin <admin@DESKTOP-MGHHDV3>
2023-12-08 18:30:15 +0900

    금비 - 커밋

---
+++ gb2023501/src/main/java/com/choongang/gb2023501/model/HwSend.java
@@ -0,0 +1,12 @@
+package com.choongang.gb2023501.model;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class HwSend {
+	private int h_num;		// 숙제번호
+	private int m_num;		// 학습자_회원번호
+	private Date hs_date;	// 교육자 전송일자
+}
