e56cf18624e29c3c238fe372a073f6ba58fe0498
awg3200 <144098993+awg3200@users.noreply.github.com>
2023-12-08 15:19:42 +0900

    지혜-member dto생성

--- gb2023501/src/main/java/com/choongang/gb2023501/model/Member.java
+++ gb2023501/src/main/java/com/choongang/gb2023501/model/Member.java
@@ -1,8 +1,27 @@
 package com.choongang.gb2023501.model;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class Member {
+	private int 	m_num;
+	private String 	m_name;
+	private String 	m_id;
+	private String 	m_pswd;
+	private int 	m_category;
+	private int 	m_mship_type;
+	private String 	m_phone;
+	private String 	m_tel;
+	private String 	m_birth;
+	private int 	m_gender;
+	private String 	m_email;
+	private String 	m_address;
+	private Date 	m_regi_date;
+	private Date 	m_modi_date;
+	private int 	m_econsent;
+	private int 	m_sconsent;
+	private int 	m_dele_status;
 
 }
