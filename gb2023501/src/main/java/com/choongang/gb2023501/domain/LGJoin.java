35f0c71f60c1082e7fc30e8c0832f7a30a8abcb8
admin <admin@DESKTOP-2O3K7EH>
2023-12-08 17:58:12 +0900

    나는 바보

--- gb2023501/src/main/java/com/choongang/gb2023501/domain/LGJoin.java
+++ gb2023501/src/main/java/com/choongang/gb2023501/domain/LGJoin.java
@@ -1,11 +1,9 @@
 package com.choongang.gb2023501.domain;
 
 import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
 import lombok.Data;
 
 @Entity
@@ -17,20 +15,20 @@ public class LGJoin {
 	private int  lgNum;
 	
 	// 학습자_회원번호
-	@Id
-	@Column(name = "lg_num")
+	// @Id
+	@Column(name = "m_num")
 	private int  mNum;
 	
 	// 신청일자
-	@Column(name = "lg_num")
+	@Column(name = "lgd_joindate")
 	private Date lgdJoindate;
 	
 	// 승인여부
-	@Column(name = "lg_num")
+	@Column(name = "lgd_approval")
 	private int  lgdApproval;
 	
 	// 승인일자
-	@Column(name = "lg_num")
+	@Column(name = "lgd_appdate")
 	private Date lgdAppdate;
 
 }
