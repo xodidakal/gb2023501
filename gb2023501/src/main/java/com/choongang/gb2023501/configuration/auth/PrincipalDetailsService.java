d939d630f99b6fe7493a48c473f6f5d8837e374f
awg3200 <144098993+awg3200@users.noreply.github.com>
2023-12-13 02:57:14 +0900

    지혜 시큐리티...

--- gb2023501/src/main/java/com/choongang/gb2023501/configuration/auth/PrincipalDetailsService.java
+++ gb2023501/src/main/java/com/choongang/gb2023501/configuration/auth/PrincipalDetailsService.java
@@ -2,6 +2,7 @@ package com.choongang.gb2023501.configuration.auth;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,15 +20,17 @@ import lombok.extern.slf4j.Slf4j;
 //@Service라고 하면 PrincipalDetailsService가 IoC로 등록됨 따라서 자동으로 loadUserByUsername가 자동으로 호출 됨
 @Service
 @Slf4j
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService {
 
-	private final MemberRepository mr;
+	@Autowired
+	private MemberRepository mr;
 
+//	두번째방법
 //	@Override
 //	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 //		//nullPoint 방지 위해 Optional사용
-//		Optional<Member> op = mr.findById(username);
+//		Optional<Member> op = mr.findByMmId(username);
 //		if(!op.isPresent()) {
 //			throw new UsernameNotFoundException("사용자 없음");
 //		}
@@ -39,39 +42,54 @@ public class PrincipalDetailsService implements UserDetailsService {
 //		//지금은 User상속받은 SecurityUser로 리턴함
 //		return new SecurityUser(member);
 //	}
-//	
+////	
 	
-	
-	
-	
-	//첫번 째 영상보고 한 것 -> 실패
-	//시큐리티 session = Autentication = UserDetails
+//	//시큐리티 session = Autentication = UserDetails
+//	//시큐리티가 로그인 가로챌 떄 id랑 비번가로채는데 비번은 알아서 처리함 -> username만 db에 있는지 확인하면 됨 
+//	//loadUserByUsername를 오버라이딩해야 PrincipalDetails에 내 Member정보 담을 수 있음
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String mmId) throws UsernameNotFoundException {
 		
-		System.out.println("username : " + username);
-		//주의!! 
-		//loadUserByUsername의 파라미터 username는 반드시 로그인폼 input태그의 name속성이 name="username"이어야만 매칭이 됨
-		//만약 name을 username이 아닌 다른 것을 쓰고 싶다면 
-		//SecurityConfig에서 .usernameParameter("m_id")로 따로 설정해줘야 함 안그러면 name속성이 username와 다를경우 매칭 안되는 문제 생김
+		System.out.println("username(mmId) : " + mmId);
 		
 		//로그인 폼에서 로그인 버튼을 누르면 /login이 발동되고 스프링은 IoC컨테이너에서 UserDetailsService 타입으로 등록된 것을 찾게 됨
 		//따라서 PrincipalDetailsService를 찾고 loadUserByUsername함수를 찾고 username로 넘어온 파라미터를 가지고 옴(여기선 m_id로 따로 설정을 해줌)
 		
 		//해당 파라미터(여기선 m_id)를 가진 사용자가 있는지 확인
-		//mr엔 기본적인 crud에 관한 메소드만 있으므로 따라서 username으로 검색하려면 findByUsername는 새로 만듦
-		Member memberEntity = mr.findByMmId(username); 
+		Member memberEntity = mr.findByMmId(mmId)
+								.orElseThrow(()-> {
+													return new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다. : " + mmId);
+								}); 
 		
-		//검색해서 가져온 유저네임이 null이 아니면 사용자가 있다는 것이고 그 사용자를 PrincipalDetails에 넣음 없으면 null을 리턴
-		if(memberEntity != null) {
-			return new PrincipalDetails(memberEntity);
-			//사용자의 m_id를 PrincipalDetails에 담아서 리턴하면 (PrincipalDetails의 맨 위 주석내용 참고) 
-			//PrincipalDetails은 UserDetails을 상속 받았으므로 Authentication에 넣을 수 있고 Authentication은 Security Session에 담을 수 있음
-			//즉 Security Session(Authentication(PrincipalDetails))이 되는 것
-		}
+//		//검색해서 가져온 유저네임이 null이 아니면 사용자가 있다는 것이고 그 사용자를 PrincipalDetails에 넣음 없으면 null을 리턴
+//		if(memberEntity != null) {
+//			return new PrincipalDetails(memberEntity);
+//			//사용자의 m_id를 PrincipalDetails에 담아서 리턴하면 (PrincipalDetails의 맨 위 주석내용 참고) 
+//			//즉 Security Session(Authentication(PrincipalDetails))이 되는 것
+//		}
 
-		return null;
+		//시큐리티 세션에 유저 정보 저장됨
+		return new PrincipalDetails(memberEntity);
 	}
 	
 
+//	//첫번 째 영상보고 한 것 -> 실패
+//	//시큐리티 session = Autentication = UserDetails
+//	// login url 캐치 -> 로그인 인증 작업 수행 -> 유저정보 조회 기능 호출
+//	@Override
+//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		
+//		System.out.println("username : " + username);
+//		
+//		// username 으로 사용자 정보 조회
+//		Member memberEntity = mr.findByMmId(username); 
+//		
+//		// 사용자 정보가 잇으면 권한 세팅 
+//		if(memberEntity == null) {
+//			throw new UsernameNotFoundException("User don't exist1");
+//		}
+//
+//		return new PrincipalDetails(memberEntity);
+//	}
+	
 }
