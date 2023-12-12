3ec2eb92fc6c2e0308cda71c466dbbe776e0bc91
admin <admin@DESKTOP-MGHHDV3>
2023-12-12 19:19:22 +0900

    금비 - 숙제 전송 커밋

---
+++ gb2023501/src/main/java/com/choongang/gb2023501/gbService/GbLgJoinService.java
@@ -0,0 +1,15 @@
+package com.choongang.gb2023501.gbService;
+
+import java.util.List;
+
+import com.choongang.gb2023501.model.Homework;
+import com.choongang.gb2023501.model.HwSend;
+import com.choongang.gb2023501.model.LearnGrp;
+import com.choongang.gb2023501.model.LgJoin;
+
+public interface GbLgJoinService {
+
+	List<LearnGrp> 	selectLgJoinList(Homework homework);
+	List<LgJoin> 	selectLgJoinMemberList(HwSend hwsend); 
+
+}
--- gb2023501/src/main/java/com/choongang/gb2023501/gbService/HomeworkService.java
+++ gb2023501/src/main/java/com/choongang/gb2023501/gbService/HomeworkService.java
@@ -3,10 +3,10 @@ package com.choongang.gb2023501.gbService;
 import java.util.List;
 
 import com.choongang.gb2023501.model.Homework;
+import com.choongang.gb2023501.model.LgJoin;
 
 public interface HomeworkService {
 	int 			selectHomeworkListCnt(Homework homework);
 	List<Homework> 	selectHomeworkList(Homework homework);
 	int 			insertUpdateHomework(Homework homework);
-
 }
