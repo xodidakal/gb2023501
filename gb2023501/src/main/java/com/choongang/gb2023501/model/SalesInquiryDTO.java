e22f7a9fdbf764d2e4374d134947e74c3e7c7156
admin <admin@DESKTOP-UU4I217>
2023-12-13 19:20:23 +0900

    용 컴

---
+++ gb2023501/src/main/java/com/choongang/gb2023501/model/SalesInquiryDTO.java
@@ -0,0 +1,20 @@
+package com.choongang.gb2023501.model;
+
+
+import java.util.Date;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SalesInquiryDTO {
+
+	private Date  goOrderDate;
+//	private int	 goPayment;
+	private long  salesCnt;
+	private long  salesSum;
+}
