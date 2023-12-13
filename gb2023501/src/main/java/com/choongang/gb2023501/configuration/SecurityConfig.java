1205b0f314bf3e716c69e99ba4af3b07a4f2dbc3
awg3200 <144098993+awg3200@users.noreply.github.com>
2023-12-13 09:33:41 +0900

    지혜 시큐리티 설정 변경

--- gb2023501/src/main/java/com/choongang/gb2023501/configuration/SecurityConfig.java
+++ gb2023501/src/main/java/com/choongang/gb2023501/configuration/SecurityConfig.java
@@ -4,6 +4,8 @@ package com.choongang.gb2023501.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,14 +26,15 @@ public class SecurityConfig {
 	}
 	
 	@Autowired
-	private UserDetailsServiceImpl usi;
+//	private UserDetailsServiceImpl usi;
+	private  CustomAuthenticationProvider authProvider;
 	
 	// Security 모듈을 비활성화 하겠다.
 //	WebSecurityConfigurerAdapter는  Deprecated 되었으므로 사용하지 않고 아래와 같이 SecurityFilterChain을 Bean으로 등록하여 사용 
 	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.userDetailsService(usi); // UserDetailsServiceImpl 등록
+//		http.userDetailsService(usi); // UserDetailsServiceImpl 등록
 		http.authorizeRequests() //인증, 인가가 필요한 URL 지정
 //			.antMatchers("/user/**").authenticated() //authenticated() : 해당 URL에 진입하기 위해서 Authentication(인증, 로그인)이 필요 -> "/user/**"이런 주소로 들어오면 인증 필요
 //			.antMatchers("/admin/**").hasRole("ADMIN") //hasAuthority() : 해당 URL에 진입하기 위해서 Authorization(인가, ex)권한이 ADMIN인 유저만 진입 가능)이 필요 -> "/admin/**" 이런 주소로 들어오면 ROLE_ADMIN권한 필요 
@@ -50,11 +53,18 @@ public class SecurityConfig {
 			.logoutSuccessUrl("/") //로그 아웃 성공시 이동할 URL (메이페이지로 이동)
 			
 			;
-		
+		// Authentication Provider 등록.
+		http.authenticationProvider(authProvider);
 		
 		return http.build();
 	}
 	
 
+	@Bean
+	AuthenticationManager authenticationManager(
+		AuthenticationConfiguration authenticationConfiguration
+	) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
 
 }
