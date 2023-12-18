553066b6c117965a68d3e230aa72e7cc1b63110c
admin <admin@DESKTOP-2O3K7EH>
2023-12-18 21:34:48 +0900

    희라 12/18 학원

--- gb2023501/src/main/java/com/choongang/gb2023501/hrService/LearnGrpService.java
+++ gb2023501/src/main/java/com/choongang/gb2023501/hrService/LearnGrpService.java
@@ -10,8 +10,11 @@ import com.choongang.gb2023501.model.Game;
 public interface LearnGrpService {
 	// 교육자마당 > 내학습그룹 (SELECT / JPA)
 	// 교육자마당 > 학습그룹 상세 (SELECT / JPA) - 학습그룹 정보
-	 List<LearnGrpDTO> learnGroupList(int lg_num, String sort, String type, String keyword);
+	List<LearnGrpDTO> learnGroupList(int lg_num, String sort, String type, String keyword);
 	
+	// 교육자마당 > 내학습그룹 (DELETE / JPA)
+	void learnGroupListDelete(int lg_num);
+
 	// 교육자마당 > 학습그룹 등록 - 화면 (SELECT / MyBatis)
 	List<Game> learnGroupForm(int g_num);
 
@@ -20,4 +23,5 @@ public interface LearnGrpService {
 
 	// 교육자마당 > 학습그룹 상세 (SELECT / JPA) - 학습자 명단
 	List<MemberDTO> joinedMemberList(int lg_num);
+
 }
